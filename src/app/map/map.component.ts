6832c323a1fc675efff260751b7638c899922dd8
Dražen Lučanin <kermit666@gmail.com>
2021-05-01 22:28:51 +0200

    add example cluster group

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import * as Leaflet from 'leaflet';
+import { MarkerClusterGroup } from "leaflet.markercluster";
 import { Component, OnInit, AfterContentInit } from '@angular/core';
 
 const views = {
@@ -37,6 +38,12 @@ export class MapComponent implements OnInit, AfterContentInit {
           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         })
       .addTo(this.map);
+      
+      var markers = new MarkerClusterGroup();
+      markers.addLayer(Leaflet.marker([45.8107711,15.9516637]));
+      markers.addLayer(Leaflet.marker([45.810,15.951]));
+      markers.addLayer(Leaflet.marker([45.810196,15.9688975]));
+      this.map.addLayer(markers);
       
   }
 
