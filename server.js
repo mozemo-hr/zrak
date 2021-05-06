64770df0021648fb6a0a5e793b960be92454ff97
Domagoj Nadramija <domagoj.nadramija@t-matix.com>
2021-05-06 23:33:27 +0200

    Use express-sslify

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const path = require('path');
+const enforce = require('express-sslify');
 
 const app = express();
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/zrak'));
-
-// redirect to https on Heroku
-// https://stackoverflow.com/a/23894573
-app.use(function (req, res, next) {
-  console.log("Headers", req.headers);
-  if (
-    req.headers['x-forwarded-proto'] &&
-    req.headers['x-forwarded-proto'] !== 'https'
-  )
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  else return next(); /* Continue to other routes if we're not redirecting */
-});
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/zrak/index.html'));
