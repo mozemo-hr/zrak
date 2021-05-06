f5117e1f5d788deaedace05e9e5e4f0e4853df9b
Domagoj Nadramija <domagoj.nadramija@t-matix.com>
2021-05-06 20:45:58 +0200

    Header with links first draft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './components/about/about.component';
+import { MapComponent } from './components/map/map.component';
+
+const routes: Routes = [
+  { path: 'inicijativa', component: AboutComponent },
+  { path: 'zasto-mjerimo', component: AboutComponent },
+  { path: 'legenda', component: AboutComponent },
+  { path: '**', component: MapComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
