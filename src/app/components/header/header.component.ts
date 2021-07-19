944c6f53efa99be394d30f9cfb8539d88e4d24e9
dviskovic <87649516+dviskovic@users.noreply.github.com>
2021-07-19 13:47:47 +0200

    Update header.component.ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,16 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public current: string;
+  public routes: Record<string, string> = {};
+  
   @Input() showLegend: boolean;
-  constructor() {}
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    for (let route of this.router.config.filter(x => x.path != "**")) {
+      this.routes[route.path] = route.data.name;
+    }
+    this.current = this.router.url.replace("/", "");
+  }
 }
